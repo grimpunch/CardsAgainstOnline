0b13e94e4b64fb271ceaa3d2bad0770e72ce0b22
Christian Murphy <christheromguy@googlemail.com>
2015-02-27 18:53:28 +0000

    Added documentation for the CAHPlayer class. Implemented hand_size property

--- CardsAgainstHumanity/__init__.py
+++ CardsAgainstHumanity/__init__.py
@@ -1,8 +1,9 @@
-__author__ = 'christian'
-
-
-class CAHPlayer:
-    def get_hand_size(self):
+class CAHPlayer(object):
+    """
+    Represents a player in the game for the game logic portion of the server.
+    """
+    @property
+    def hand_size(self):
         return len(self.hand)
 
     def __init__(self, name=None):
@@ -10,4 +11,3 @@ class CAHPlayer:
         self.awesome_points = 0
         self.isCzar = False
         self.name = name
-        self.hand_size = self.get_hand_size()
