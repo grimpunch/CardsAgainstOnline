dd4d7fa80ade04fa8b07e8f7a169cdaf8df3e038
Christian Murphy <christheromguy@googlemail.com>
2015-02-27 22:14:16 +0000

    CAHPlayer class expanded , creating Game class for game specific functions

--- CardsAgainstHumanity/GameHandler.py
+++ CardsAgainstHumanity/GameHandler.py
@@ -2,12 +2,13 @@ import random
 from CardsAgainstHumanity.card_data import CardParser
 from CardsAgainstHumanity import CAHPlayer
 
+
 class CardHandler():
     def __init__(self):
         self.card_db = CardParser()
         self.black_deck = self.create_deck(card_type=r'Q')
         self.white_deck = self.create_deck(card_type=r'A')
-        self.players = []
+        self.discarded_white_cards = []
 
     def create_deck(self, card_type=None):
         """
@@ -26,28 +27,71 @@ class CardHandler():
         return deck
 
     def draw_hand(self, player):
+        """
+        For the player specified, draw back up to 10 cards.
+        :param player:
+        :return:
+        """
         cards_to_draw = (10 - player.hand_size)
         if self.white_deck.count() < cards_to_draw:
-            return NotImplemented('Need to implement white deck reshuffle')
+            self.shuffle_discards_into_white_deck()
         for _ in range(cards_to_draw):
             player.hand.add(self.white_deck.pop())
         assert player.hand_size == 10
         return
 
-    def add_player(self, playerName=None):
-        player = CAHPlayer(name=playerName)
-        self.draw_hand(player)
-        print('%s , has entered the game' % player)
+    def discard(self, card=None):
+        self.discarded_white_cards.append(card)
         return
 
+    def shuffle_discards_into_white_deck(self):
+        self.white_deck.append(self.discarded_white_cards)
+        random.shuffle(self.white_deck)
+        return
+
+
+class Game():
+    def __init__(self):
+        self.cards = CardHandler()
+        self.discards = self.cards.discarded_white_cards
+        self.players = []
+        self.round = 0
+        self.black_deck = self.cards.black_deck
+        self.white_deck = self.cards.white_deck
+
+    def add_player(self, player_name=None):
+        player = CAHPlayer(name=player_name)
+        self.cards.draw_hand(player)
+        print('%s, has entered the game' % player)
+        return
+
+    def remove_player(self, player_name=None):
+        quitter = self.get_player_by_name(player_name)
+        quitter.hand = []
+        quitter.awesome_points = 0
+        self.players.remove(quitter)
+        return
+
+    def get_player_by_name(self, player_name=None):
+        """
+        Return player from game's player list via player's name.
+        :type players: CAHPlayer
+        """
+        return [player for player in self.players if player_name in player.name]
 
     def new_game(self):
         """
         Initialises a new game
         :return:
         """
-
+        self.black_deck = self.cards.create_deck(card_type='Q')
+        self.white_deck = self.cards.create_deck(card_type='A')
+        for player in self.players:
+            self.cards.draw_hand(player)
         return
 
     def clean_up(self):
-        return NotImplemented
+        for player in self.players:
+            player.hand = []
+            player.awesome_points = 0
+        return
