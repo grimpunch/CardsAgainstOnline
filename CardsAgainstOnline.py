d2c34b95f0428c7e8998a5b691083c268664673c
Christian Murphy <christheromguy@googlemail.com>
2015-09-21 13:52:35 +0100

    Streamlined Repo to remove all traces of tornado

--- CardsAgainstOnline.py
+++ CardsAgainstOnline.py
@@ -1,12 +1,7 @@
-from CardsAgainstGame import GameHandler
 from CardsAgainstGame.GameHandler import Game
-from Server import Room
 import json
 import uuid
 import os
-# import pymongo
-# import bcrypt
-# import hashlib
 import urllib
 import base64
 from flask_interface import app
@@ -17,21 +12,6 @@ class Application():
     def __init__(self):
         self.game = Game()
         self.clients = {}
-        handlers = [
-        url(r'/', HelloHandler, name='index'),
-        url(r'/login', LoginHandler, name='login'),
-        url(r'/play', GameScreenHandler, name='game'),
-        url(r'/host', GameScreenHandler, name='host'),
-        url(r'/hand', HandHandler, name='hand'),
-        # url(r'/hello', HelloHandler, name='hello'),
-        # url(r'/email', EmailMeHandler, name='email'),
-        # url(r'/message', MessageHandler, name='message'),
-        # url(r'/thread', ThreadHandler, name='thread_handler'),
-        # url(r'/login_no_block', NoneBlockingLogin, name='login_no_block'),
-        # url(r'/register', RegisterHandler, name='register'),
-        # url(r'/logout', LogoutHandler, name='logout'),
-        # url(r'/chat', WebSocketChatHandler, {'clients': self.clients}, name='wbchat'),
-        ]
         self.rooms = []
         app.run()
 
