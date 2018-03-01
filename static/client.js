550b81a50222ebeef7f8086f2be39d1cab3d8d3d
Christian Murphy <christheromguy@googlemail.com>
2018-03-01 02:20:07 +0000

    removed unslider , replaced with dragend, better card dragging resulted.

--- static/client.js
+++ static/client.js
@@ -27,38 +27,54 @@ const TURN_STATE_RESULTS = 4;
 var hand_slider = null;
 
 function load_player_hand() {
-    $('.hand_area').load('/hand', function (data) {
-        var hand_banner = $('.hand_banner');
-        var slidey = hand_banner.unslider({
-            dots: true,
-            complete: function (el) {
-                if (slider_data) {
-                    slider_data.items.removeClass('active');
-                    $(slider_data.items[slider_data.current]).addClass('active');
-                    white_card_id = el.find('.active').children("div").attr("id");
+    $('.hand_area').load('/hand', function(data) {
+        $(function() {
+            var hand_banner = $('.hand_banner');
+
+            var firstChild = $(".hand_banner li:first-child").clone(),
+                lastChild = $(".hand_banner li:last-child").clone(),
+                container = $(".hand_banner ul");
+
+            firstChild.appendTo(container);
+            lastChild.prependTo(container);
+            hand_slider = hand_banner;
+            hand_banner.dragend({
+                pageClass: "hand_card",
+                direction: "horizontal",
+                duration: 120,
+                onSwipeEnd: function() {
+                    var first = this.pages[0],
+                        last = this.pages[this.pages.length - 1];
+
+                    if (first === this.activeElement) {
+                        this.jumpToPage(this.pages.length - 1);
+                    }
+
+                    if (last === this.activeElement) {
+                        this.jumpToPage(2);
+                    }
+
+                    white_card_id = this.activeElement.childNodes[0].id;
+                },
+                afterInitialize: function() {
+                    this.container.style.visibility = "visible";
+                    white_card_id = this.activeElement.childNodes[0].id;
                 }
-            }
+            });
         });
-        var slider_data = slidey.data('unslider');
-        if (slider_data) {
-            slider_data.items.removeClass('active');
-            $(slider_data.items[slider_data.current]).addClass('active');
-            white_card_id = slidey.find('.active').children("div").attr("id");
-        }
-        hand_slider = slidey;
     });
 }
 
-function get_current_player_count(){
-     $.get('/player_count', function(data) {
-         player_count = data['player_count'];
-         var player_count_header = $('#player_count_header');
-         console.log("Player count == " + player_count);
-         player_count_header.text('Players: ' + player_count);
-     });
+function get_current_player_count() {
+    $.get('/player_count', function(data) {
+        player_count = data['player_count'];
+        var player_count_header = $('#player_count_header');
+        console.log("Player count == " + player_count);
+        player_count_header.text('Players: ' + player_count);
+    });
 }
 
-function set_current_player_count(players){
+function set_current_player_count(players) {
     player_count = players;
     console.log('players ' + player_count);
     var player_count_header = $('#player_count_header');
@@ -66,59 +82,58 @@ function set_current_player_count(players){
 }
 
 var client_init = function(callback_func) {
-    $.when( $.get('/user'), $.get('/lobby_state')).then(
-        function(d1,d2) {
+    $.when($.get('/user'), $.get('/lobby_state')).then(
+        function(d1, d2) {
             username = d1[0].name;
             user_id = d1[0].id;
             lobby_state = d2[0].lobby_state;
-        if (window.location.pathname != '/host') {
-            load_player_hand();
-            callback_func();
-        } else {
-            // HOST pregame flow
-            user_is_host = true;
-            $('#hand_header').hide();
-            $('.hand_banner').hide();
-            $("#submit_white_card_button").hide();
-            $.get("/address", function (data) {
+            if (window.location.pathname != '/host') {
+                load_player_hand();
+                callback_func();
+            } else {
+                // HOST pregame flow
+                user_is_host = true;
+                $('#hand_header').hide();
+                $('.hand_banner').hide();
+                $("#submit_white_card_button").hide();
+                $.get("/address", function(data) {
                     console.log(data);
                     if (lobby_state == LOBBY_STATE_WAITING_FOR_GAME_START) {
                         console.log('host requested server address for display');
                         var join_header = $('#join_header');
                         var address = data.split("or ")[1];
                         join_header.append('<a href="' + address + '" target="_blank">Go to ' + data +
-                        ' now!');
+                            ' now!');
                         join_header.show();
                         user_id = '0000';
                         username = 'HOST';
                     }
-            }).then(function () {
-                get_current_player_count();
-                callback_func();
-            });
-        }
-        get_czar();
-    });
+                }).then(function() {
+                    get_current_player_count();
+                    callback_func();
+                });
+            }
+            get_czar();
+        });
 };
 
-function get_czar(){
-    $.get("/czar", function (data) {
+function get_czar() {
+    $.get("/czar", function(data) {
         var czar_header = $('#current_czar_header');
         var json_resp = data;
         console.log('Czar: ' + data);
-        if (json_resp['czar_chosen'] == true){
+        if (json_resp['czar_chosen'] == true) {
             czar_header.show();
             czar_header.text('Czar: ' + json_resp['czar']);
             czar_header.after('<span>' + json_resp['current_black_card_text'] + '</span>');
-        }
-        else{
+        } else {
             czar_header.hide();
         }
     });
 }
 
 function delete_cookie(name) {
-  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
 function checkCookie() {
@@ -132,7 +147,7 @@ function checkCookie() {
 
 var checkKillSwitch = setInterval(checkCookie, 2000);
 
-$(document).ready(function(){
+$(document).ready(function() {
     console.log("client ready to connect");
     checkCookie();
     namespace = '/ws'; // change to an empty string to use the global namespace
@@ -142,17 +157,20 @@ $(document).ready(function(){
     server.on('connect', function() {
         checkCookie();
         console.log('a client connected to the server');
-        client_init(function () {
+        client_init(function() {
             console.log(username + ' initialised');
-            server.emit('user_connected',
-                    { type: 'client_connect', user_id: user_id, user: username});
+            server.emit('user_connected', {
+                type: 'client_connect',
+                user_id: user_id,
+                user: username
+            });
         });
     });
 
-    server.on('player_connected', function (data) {
-       // On player connected
-        console.log('username: '+ data['username']);
-        console.log('player_count: '+ data['player_count']);
+    server.on('player_connected', function(data) {
+        // On player connected
+        console.log('username: ' + data['username']);
+        console.log('player_count: ' + data['player_count']);
         set_current_player_count(data['player_count']);
     });
 
@@ -165,8 +183,8 @@ $(document).ready(function(){
     //    document.cookie = 'username=; path=/; domain='+document.domain+'; expires=' + new Date(0).toUTCString();
     //});
 
-    server.on('czar_chosen', function(event){
-       // When server says czar is chosen, show this on the clients.
+    server.on('czar_chosen', function(event) {
+        // When server says czar is chosen, show this on the clients.
         get_czar();
     });
 
@@ -174,13 +192,17 @@ $(document).ready(function(){
         window.location.href = destination;
     });
 
-    $("#submit_white_card_button").click(function(){
+    $("#submit_white_card_button").click(function() {
         // need to handle case where white_card_id isn't set on load.
-        server.emit("user_submit_white_card",
-                   { user_id: user_id,
-                     user: username,
-                     submitted_white_card_id: white_card_id
-                   });
+        server.emit("user_submit_white_card", {
+            user_id: user_id,
+            user: username,
+            submitted_white_card_id: white_card_id
+        });
+        var sel = '#'+white_card_id;
+        $(sel)[0].className += " submit-anim";
+        setTimeout(function(){
+            $('.hand_banner').dragend("left");
+        }, 800);
     });
-
 });
